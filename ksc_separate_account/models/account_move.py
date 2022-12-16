d838ac20d09a91d8ad2f0bb0d53cb57c55193069
Hiren Dangar <hiren.dangar@konsultoo.com>
2022-12-16 10:14:36 +0530

    [ADD]Added the ksc_separate_account module to separate account for Amazon and BigCommerce sales.

---
+++ ksc_separate_account/models/account_move.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.move.line"
+
+    def _get_computed_account(self):
+        res = super(AccountInvoiceLine, self)._get_computed_account()
+        if self._context.get('team_id'):
+            sales_team = self.env['crm.team'].browse(
+                self._context.get('team_id') or self._context['team_id'])
+            if sales_team.name == 'BigCommerce':
+                bigcomm_sale_acc = self.env['account.account'].search([('code', '=', '4040')])
+                return bigcomm_sale_acc
+            elif sales_team.name == 'Amazon':
+                amazon_sale_acc = self.env['account.account'].search([('code', '=', '4030')])
+                return amazon_sale_acc
+        return res
