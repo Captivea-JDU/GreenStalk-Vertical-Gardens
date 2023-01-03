5f5a7fa3e6d980ea65de695146f62c4411c36cec
Hiren Dangar <hiren.dangar@konsultoo.com>
2023-01-03 11:35:28 +0530

    [IMP]Improved code to get account based on the auto sync process.

--- ksc_separate_account/models/account_move.py
+++ ksc_separate_account/models/account_move.py
@@ -8,14 +8,19 @@ class AccountInvoiceLine(models.Model):
     _inherit = "account.move.line"
 
     def _get_computed_account(self):
-        res = super(AccountInvoiceLine, self)._get_computed_account()
-        if self._context.get('team_id'):
+        ctx = self._context
+        sales_team = False
+        if ctx.get('team_id'):
             sales_team = self.env['crm.team'].browse(
                 self._context.get('team_id') or self._context['team_id'])
-            if sales_team.name == 'Greenstalk Vertical Gardens':
-                bigcomm_sale_acc = self.env['account.account'].search([('code', '=', '4040')])
-                return bigcomm_sale_acc
-            elif sales_team.name == 'Amazon':
-                amazon_sale_acc = self.env['account.account'].search([('code', '=', '4030')])
-                return amazon_sale_acc
-        return res
+        elif ctx.get('order'):
+            sales_team = ctx['order'].team_id
+
+        if sales_team and sales_team.name == 'Greenstalk Vertical Gardens':
+            bigcomm_sale_acc = self.env['account.account'].search([('code', '=', '4040')])
+            return bigcomm_sale_acc
+        elif sales_team and sales_team.name == 'Amazon':
+            amazon_sale_acc = self.env['account.account'].search([('code', '=', '4030')])
+            return amazon_sale_acc
+
+        return super(AccountInvoiceLine, self)._get_computed_account()
