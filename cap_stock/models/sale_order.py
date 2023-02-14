83221a20e20fe808c9d20ec701af0b0612a3f9b6
Dhaval Panchal <dhaval.panchal@konsultoo.com>
2023-02-14 10:37:33 +0530

    [IMP] cap_stock: improve code for Billing mismatch Address

--- cap_stock/models/sale_order.py
+++ cap_stock/models/sale_order.py
@@ -15,7 +15,8 @@ class SaleOrder(models.Model):
                 if large_order_tag:
                     result.tag_ids = [(5, 0, 0),(6, 0, large_order_tag.ids)]
 
-            if result.partner_invoice_id.parent_id != result.partner_shipping_id.parent_id and result.amount_total > 150:
+            # if result.partner_invoice_id.parent_id != result.partner_shipping_id.parent_id and result.amount_total > 150:
+            if (result.partner_invoice_id.street != result.partner_shipping_id.street or result.partner_invoice_id.city != result.partner_shipping_id.city or result.partner_invoice_id.state_id != result.partner_shipping_id.state_id or result.partner_invoice_id.zip != result.partner_shipping_id.zip or result.partner_invoice_id.country_id != result.partner_shipping_id.country_id) and result.amount_total > 150:
                 billing_address_mismatch_tag = self.env['crm.tag'].search([('name', '=', 'Billing Address Mismatch')])
                 if billing_address_mismatch_tag:
                     result.tag_ids = [(5, 0, 0),(6, 0, billing_address_mismatch_tag.ids)]
@@ -43,7 +44,8 @@ class SaleOrder(models.Model):
                     if large_order_tag:
                         result.tag_ids = [(5, 0, 0),(6, 0, large_order_tag.ids)]
 
-                if result.partner_invoice_id.parent_id != result.partner_shipping_id.parent_id and result.amount_total > 150:
+                # if result.partner_invoice_id.parent_id != result.partner_shipping_id.parent_id and result.amount_total > 150:
+                if (result.partner_invoice_id.street != result.partner_shipping_id.street or result.partner_invoice_id.city != result.partner_shipping_id.city or result.partner_invoice_id.state_id != result.partner_shipping_id.state_id or result.partner_invoice_id.zip != result.partner_shipping_id.zip or result.partner_invoice_id.country_id != result.partner_shipping_id.country_id) and result.amount_total > 150:
                     billing_address_mismatch_tag = self.env['crm.tag'].search([('name', '=', 'Billing Address Mismatch')])
                     if billing_address_mismatch_tag:
                         result.tag_ids = [(5, 0, 0),(6, 0, billing_address_mismatch_tag.ids)]
