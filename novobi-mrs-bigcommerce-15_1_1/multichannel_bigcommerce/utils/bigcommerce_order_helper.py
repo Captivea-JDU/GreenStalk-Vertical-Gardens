7fab189e44a886d36ce22fcf16d15633df467437
Dhaval Panchal <dhaval.panchal@konsultoo.com>
2023-03-08 00:05:06 +0530

    [IMP] multichannel_bigcommerce: improve code to import order comments from bigcommerce order

--- novobi-mrs-bigcommerce-15_1_1/multichannel_bigcommerce/utils/bigcommerce_order_helper.py
+++ novobi-mrs-bigcommerce-15_1_1/multichannel_bigcommerce/utils/bigcommerce_order_helper.py
@@ -115,6 +115,7 @@ class SingularOrderDataInTrans(common_formatter.DataTrans):
             'channel_order_ref': order['customer_reference'],
             'payment_gateway_code': order['payment_method'],
             'staff_notes': order['staff_notes'],
+            'customer_message': order['customer_message'],
             'status_id': order['status_id'],
             'currency_code': order['currency_code'],
         }
